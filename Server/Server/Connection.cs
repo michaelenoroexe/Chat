8c2480c8ba6a94fb32113691717f8510f9319c21
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-10-22 13:37:40 +0300

    Redy to recieve test info

---
+++ Server/Server/Connection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Connection
+    {
+        /// <summary>
+        /// IP adress of first user in connection
+        /// </summary>
+        public IPAddress FirstUser { get; }
+        /// <summary>
+        /// IP adress of second user in connection
+        /// </summary>
+        public IPAddress SecondUser { get; }
+
+        public Connection(IPAddress firstUser, IPAddress secondUser)
+        {
+            FirstUser = firstUser;
+
+            SecondUser = secondUser;
+        }
+        public Connection(string firstUser, string secondUser)
+        {
+            FirstUser = IPAddress.Parse(firstUser);
+
+            SecondUser = IPAddress.Parse(secondUser);
+        }
+
+        /// <summary>
+        /// Determine when specific Connection object are equal to current
+        /// </summary>
+        /// <param name="con"></param>
+        /// <returns></returns>
+        public bool Equals(Connection con)
+        {
+            return FirstUser == con.FirstUser && SecondUser == con.SecondUser;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != typeof(Connection)) return false;
+            return this.Equals((Connection)obj);
+        }
+        public override int GetHashCode()
+        {
+            return FirstUser.GetHashCode() + SecondUser.GetHashCode();
+        }
+    }
+}
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        private static Queue<IPAddress> _reqQueue = new Queue<IPAddress>();
+
+        private static Dictionary<int, Connection> _connStrings = new Dictionary<int, Connection>();
+
+        static void Main(string[] args)
+        {
+            // Preparing sockets to listen 
+            var listener = new TcpListener(IPAddress.Loopback, 5567);
+            
+            //Listening begins
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Ожидание подключений... ");
+
+                // получаем входящее подключение
+                TcpClient client = listener.AcceptTcpClient();
+
+                client.Client.RemoteEndPoint?.Serialize();
+            }
+        }
+    }
+}
