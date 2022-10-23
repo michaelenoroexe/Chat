91d5773b1b488fd1798acd4521ef4f380d6577ba
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-10-23 16:36:58 +0300

    Revert Back

--- Client/UI/MainWindow.xaml.cs
+++ Client/UI/MainWindow.xaml.cs
@@ -35,11 +35,7 @@ namespace UI
         private async Task ConnectToServer()
         {
             TcpClient tcpClient = new TcpClient();
-            // 2ip 188.190.221.175
-            // radmin 
-            //tcpClient.Connect(IPAddress.Loopback, 5567);
-
-            tcpClient.Connect(IPAddress.Parse(IP.Text), 5567); ;
+            tcpClient.Connect(IPAddress.Loopback, 5567); ;
 
             using (var stream = tcpClient.GetStream())
             {
