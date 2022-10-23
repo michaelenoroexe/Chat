0235e57f5d10828f247a71a8933209b8a151a86f
Michaelenoro <serezha.alexandrovitch2018@yandex.ru>
2022-10-23 15:10:34 +0300

    Created first test version of accept send mechanism

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -12,11 +12,12 @@ namespace Server
 
         private static Dictionary<int, Connection> _connStrings = new Dictionary<int, Connection>();
 
+        private static Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
         static void Main(string[] args)
         {
             // Preparing sockets to listen 
             var listener = new TcpListener(IPAddress.Loopback, 5567);
-            var sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             var keyBuff = new byte[4];
             
             //Listening begins
@@ -32,11 +33,27 @@ namespace Server
                 if (_connStrings.TryGetValue(client.ConnectionKey, out Connection? connect))
                 {
                     sender.SendToAsync(client.SendedTextBytes, SocketFlags.None, new IPEndPoint(client.IP, 5567));
+                    continue;
                 }
-                // TODO Checking queue for available connctions
-                // TODO Creating connection between two users
-                // TODO Set user to queue if pool is empty
+                if (_reqQueue.Any())
+                {
+                    RegisterConnection(_reqQueue.Dequeue(), client.IP);
+                    continue;
+                }
+                _reqQueue.Enqueue(client.IP);
             }
+        }
+
+        private static async Task RegisterConnection(IPAddress firstUser, IPAddress secondUser)
+        {
+            var con = new Connection(firstUser, secondUser);
+
+            _connStrings.Add(con.GetHashCode(), con);
+            var sendUs1 = sender.SendToAsync(BitConverter.GetBytes(con.GetHashCode()), SocketFlags.None, new IPEndPoint(firstUser, 5567));
+            var sendUs2 = sender.SendToAsync(BitConverter.GetBytes(con.GetHashCode()), SocketFlags.None, new IPEndPoint(secondUser, 5567));
+
+            await sendUs1;
+            await sendUs2;
         }
     }
 }
