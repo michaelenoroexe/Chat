10ccd211129b1a79e315b04af7ac6acb0aee35b1
Кальницкий Михаил <86874761+michaelenoroexe@users.noreply.github.com>
2022-11-05 18:34:19 +0300

    Client (#2)
    * Ready to send test byte
    
    * Test with changed ip
    
    * Revert Back
    
    * Changed ui layout
    
    * Change of main w
    
    * Created connect
    
    * Created message sending
    
    * Created new client structure
    
    * Deleted samples of items.

---
+++ Client/UI/Connect/IConnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UI.Connect
+{
+    internal interface IConnect<T>
+    {
+        /// <summary>
+        /// Send message to remoute host.
+        /// </summary>
+        /// <param name="message">Type of object, sended to remoute host.</param>
+        public void Send(T message);
+        /// <summary>
+        /// Occurs when message is received.
+        /// </summary>
+        public event EventHandler<MessageEventArgs<T>> MessageReceived;
+    }
+}
